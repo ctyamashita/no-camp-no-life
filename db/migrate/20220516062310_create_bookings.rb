5b4a275e22e71d6978706079188a755be0dd82cc
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-16 15:50:58 +0900

    models in progress

--- db/migrate/20220516062310_create_bookings.rb
+++ db/migrate/20220516062310_create_bookings.rb
@@ -3,7 +3,9 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.date :start_date
       t.date :end_date
-      t.integer :status
+      t.integer :status, default: 0
+      t.references :car, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
