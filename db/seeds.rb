4077f538aaed592bac4d1d76087e87e6cd0a5db2
Hakuyo Harimoto <haharimoto@gmail.com>
2022-05-16 17:27:10 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seed cars
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts "creating cars"
+user = User.new(email: "haha@gmail.com", password: "haha")
+20.times do
+  Car.create(
+    car_model: Faker::Vehicle.make_and_model,
+    capacity: rand(4..8),
+    description: Faker::Vehicle.car_options,
+    price_per_day: rand(1111..9999),
+    user: user
+  )
+end
+puts "cars created"
+
+
+# t.string :car_model
+# t.integer :capacity
+# t.text :description
+# t.integer :price_per_day
