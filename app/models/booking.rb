7da7d0b6b6745ac4b238bdd840c873b9df859723
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-16 16:16:26 +0900

    models update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
 
+  enum status: { pending: 0, confirmed: 1, rejected: 2 }
+
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :bookings
+
+  validates :price_per_day, numericality: { only_integer: true }
+  validates :capacity, numericality: { only_integer: true }
+  validates :car_model, presence: true
 end
