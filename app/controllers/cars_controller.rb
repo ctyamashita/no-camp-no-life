a93d4ad506ad29ebb919e6c7fca018fa1747ce4f
Hakuyo Harimoto <haharimoto@gmail.com>
2022-05-16 20:06:23 +0900

    added new and create in cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,23 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to cars_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:car_model, :capacity, :description, :price_per_day)
+  end
 end
