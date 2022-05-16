431771dcbdeb8949f8ea9912ef3e51922a45561a
Ricardo Momoi <shinjimomoi@gmail.com>
2022-05-16 17:08:18 +0900

    Created empty controller for bookings and controller with index method for cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,27 +2,4 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
-
-  def show
-    @car = Car.find(params[:id])
-  end
-
-  def new
-    @car = Car.new
-  end
-
-  def create
-    @car = Car.new(car_params)
-    if @car.save
-      redirect_to car_path(@car)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def car_params
-    params.require(:list).permit(:name, :capacity, :description, :price_per_day)
-  end
 end
