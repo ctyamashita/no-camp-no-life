63cb87cefa633e4f2a47c6005c80411549ba80cb
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-17 12:47:57 +0900

    Form and Booking status added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.car = Car.find(params[:car_id])
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render car_path(@booking.car)
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
