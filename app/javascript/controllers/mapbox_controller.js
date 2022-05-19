f7d1220103db164c7a17c54dea1a0e47e8682a68
Ricardo Momoi <shinjimomoi@gmail.com>
2022-05-20 01:56:39 +0900

    style done for now

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/light-v10"
+      style: "mapbox://styles/mapbox/outdoors-v11"
     })
 
     this.#addMarkersToMap()
